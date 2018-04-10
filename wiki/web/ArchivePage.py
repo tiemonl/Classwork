78f650a5cfac720432cfc7ed25b23bb34922502a
Guardryan <Guard.ryan45@yahoo.com>
2018-04-10 12:10:01 -0400

    continued maintenance of Archive system

--- wiki/web/ArchivePage.py
+++ wiki/web/ArchivePage.py
@@ -1,47 +1,39 @@
-from collections import OrderedDict
 from io import open
-import datetime
-import os
-import re
 
-from flask import abort
-from flask import url_for
-import markdown
 
 from wiki.web.ArchiveDatabaseConnection import ArchiveDatabaseConnection
 
 
 class ArchivePage:
-    def __init__(self, name, storing):
-        self.path = "../../content/" + name
+    def __init__(self, name, url):
+        self.fileName = name + ".md"
+        self.path = url
         self.file = open(self.path, "r")
-        self.fileName = self.file.name
         self.contents = ""
-        if storing:
-            for line in self.file.readlines():
-                self.contents +=  line.__str__()
-            self.contentBytes = str.encode(self.contents)
-            print(type(self.contentBytes))
-            date = datetime.datetime.now()
-            self.timestamp = date.month.__str__() + "/" + date.day.__str__() + "/" + date.year.__str__() + " " +\
-                date.hour.__str__() + ":" + date.minute.__str__() + ":" + date.second.__str__()
-            print(self.contents + "\n" + self.timestamp)
-            #print(self.contentBytes)
-        cursor = ArchiveDatabaseConnection().conn.cursor()
-        print "Insert?"
-        cursor.execute("INSERT INTO wiki.pages(page_id,commit_id,date_update,file_name, page_file)"
-                       "VALUES((Select count(page_id) + 1 from wiki.pages),1, current_date, 'home.md', '{" + self.contentBytes +"}')")
-        print "select statement?"
-        cursor.execute("Select count(*) from wiki.pages")
-        rows = cursor.fetchall()
-        for row in rows:
-            print " ", row[0]
+        for line in self.file.readlines():
+            self.contents +=  line.__str__()
+        self.contentBytes = str.encode(self.contents)
+        self.cursor = ArchiveDatabaseConnection().conn.cursor()
+        #self.cursor.execute("INSERT INTO wiki.pages(page_id,commit_id,date_update,file_name, page_file)"
+        #                    "VALUES('500','1',CURRENT_DATE, 'yes.md', '{yes}')")
+        #self.cursor.execute("COMMIT")
 
-    #def store(self):
-
-    #def retrieve(self):
+    def store(self):
+        self.cursor.execute("Select file_name from wiki.pages")
+        if self.cursor.fetchall()[0] == self.fileName:
+            self.cursor.execute("INSERT INTO wiki.pages(page_id,commit_id,date_update,file_name, page_file)"
+                       "VALUES((SELECT COUNT(page_id) + 1 from wiki.pages),1, CURRENT_DATE, \" + self.fileName + \"',"
+                       " '{" + self.contents +"}')")
+            self.cursor.execute("COMMIT")
+        else:
+            self.cursor.execute('INSERT INTO wiki.pages(page_id,commit_id,date_update,file_name, page_file)'
+                       'VALUES((SELECT (MAX(page_id) + 1) from wiki.pages where file_name = "' + self.fileName + '"),'
+                            ' (SELECT (count(commit_id) + 1) from wiki.pages where file_name = "' + self.fileName + '") '
+                                ', current_date, "' + self.fileName + '", "{' + self.contents + '}")')
+            self.cursor.execute("COMMIT")
 
 '''
 Test Code
 '''
-ArchivePage("home.md", True);
+#arc = ArchivePage("yes")
+#arc.store()
