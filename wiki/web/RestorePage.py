e8d9db4e2125fd42cbb326fc2a3c8dfa70cab0a8
tiemonl <tiemon.liam@gmail.com>
2018-04-17 20:17:08 -0400

    Testing pydoc

--- wiki/web/RestorePage.py
+++ wiki/web/RestorePage.py
@@ -5,25 +5,25 @@ from wiki.web.ArchiveDatabaseConnection import ArchiveDatabaseConnection
 
 
 
-'''
+"""
 Dev: Liam Tiemon (tiemonl1)
 
 Takes the contents of a markdown file and restores it from the database.
 Then uses ArchivePage to store the pulled page as a new page.
-'''
+"""
 class RestorePage:
-    '''
+    """
     Constructor gets the name of the file with the extensions and the url. It then reads what is in the given file.
-    '''
+    """
     def __init__(self, name, url):
         self.fileName = name
         self.path = url
         self.cursor = ArchiveDatabaseConnection().conn.cursor()
 
-    '''
+    """
     Selects the given page from the database and writes a new file with the contents from the pulled page.
     It then stores the page using ArchivePage's store function.
-    '''
+    """
     def restore(self,commitID):
         self.cursor.execute("SELECT page_file FROM wiki.page WHERE page_name='"+self.fileName+"' AND commit_id="+commitID.__str__())
 
@@ -37,9 +37,9 @@ class RestorePage:
         arc = ArchivePage(name, self.path)
         arc.store()
 
-'''
+"""
 Test Code
-'''
+"""
 # res = RestorePage("home", "/Users/liamtiemon/PycharmProjects/CSC440WikiWiki/content/home.md")
 # res.restore('home.md', 4)
 # res.cursor.execute("SELECT page_file FROM wiki.page WHERE page_file='home.md'")
